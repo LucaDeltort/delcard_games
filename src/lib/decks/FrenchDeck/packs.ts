b9e5d85c6bce7a8610fe6f40c9f81b397e7688a5
Luca Deltort <luca@deltort.net>
2026-05-13 19:09:58 +0200

    chore(dev): rename pack "Korean" to "Joseon"

--- src/lib/decks/FrenchDeck/packs.ts
+++ src/lib/decks/FrenchDeck/packs.ts
@@ -1,26 +1,26 @@
-import type { CardPack } from "$lib/decks/types";
+import type { CardPack } from '$lib/decks/types'
 
 export const frenchDeckPacks: CardPack[] = [
-  {
-    id: "default",
-    name: "Classic",
-    author: "Alborz Heydaryan",
-    basePath: "/cards/FrenchDeck/default",
-    ext: ".svg",
-  },
-  {
-    id: "cyberwave",
-    name: "Cyberwave",
-    author: "Rohit Chouhan",
-    basePath: "/cards/FrenchDeck/cyberwave",
-    ext: ".svg",
-  },
-  {
-    id: "joseon",
-    name: "Joseon",
-    basePath: "/cards/FrenchDeck/joseon",
-    ext: ".svg",
-  },
-];
+	{
+		id: 'default',
+		name: 'Classic',
+		author: 'Alborz Heydaryan',
+		basePath: '/cards/FrenchDeck/default',
+		ext: '.svg'
+	},
+	{
+		id: 'cyberwave',
+		name: 'Cyberwave',
+		author: 'Rohit Chouhan',
+		basePath: '/cards/FrenchDeck/cyberwave',
+		ext: '.svg'
+	},
+	{
+		id: 'joseon',
+		name: 'Joseon',
+		basePath: '/cards/FrenchDeck/joseon',
+		ext: '.svg'
+	}
+]
 
-export const defaultFrenchDeckPack = frenchDeckPacks[0];
+export const defaultFrenchDeckPack = frenchDeckPacks[0]
